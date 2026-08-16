2e3a42e8d2fa61734dcb79bdab9b4a3f3ff678ca
Jatin Suhas Sawant <jatinsawant02@gmail.com>
2026-08-16 21:35:20 +0530

    Worked on responsiveness of Hero section | AOS AND Category.jsx updated by a block

--- frontend/src/components/Category/Category.jsx
+++ frontend/src/components/Category/Category.jsx
@@ -2,36 +2,31 @@ import React from "react";
 import image1 from "../../assets/category/earphone.png";
 import image2 from "../../assets/category/watch.png";
 import image3 from "../../assets/category/macbook.png";
+import Button from "../Shared/Button";
 
 const Category = () => {
   return (
     <div className="py-8">
       <div className="container">
-        <div className="grid">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* first col */}
-          <div className="col-span-4">
-            <img
-              src={image1}
-              alt="Earphone"
-              className="w-full h-full object-cover"
-            />
+          <div className="py-10 pl-5 bg-gradient-to-br from-black 90 to-black/70 text-white rounded-2xl relative h-[320px] flex items-end">
+            <div>
+              <div>
+                <p>Enjoy</p>
+                <p>With</p>
+                <p>Earphone</p>
+                <Button
+                  text="Browse"
+                  bgColor="bg-primary"
+                  textColor="text-white"
+                />
+              </div>
+            </div>
           </div>
           {/* second col */}
-          <div className="col-span-4">
-            <img
-              src={image2}
-              alt="Watch"
-              className="w-full h-full object-cover"
-            />
-          </div>
+
           {/* third col */}
-          <div className="col-span-4">
-            <img
-              src={image3}
-              alt="Macbook"
-              className="w-full h-full object-cover"
-            />
-          </div>
         </div>
       </div>
     </div>
