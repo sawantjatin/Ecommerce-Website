f007b6797cd0b37672e9a33e93531d03f67aaec9
Jatin Suhas Sawant <jatinsawant02@gmail.com>
2026-08-18 22:07:04 +0530

    Category Section Completed : Category | Category2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Category from "./components/Category/Category";
+import Category2 from "./components/Category/Category2";
 
 const App = () => {
   useEffect(() => {
@@ -19,6 +20,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Category />
+      <Category2 />
     </div>
   );
 };
